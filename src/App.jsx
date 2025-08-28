f341667a8d61aa9d299e6d8ec895392f0d91cb13
huzaifashoukat07 <huzafa459@gmail.com>
2025-08-28 17:06:49 +0500

    Forget Password Screen Created

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/forgot" element={<Forgot />} />
       </Routes>
     </>
